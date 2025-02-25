a5a66463dd177f30f5863278c8c1888d133bdb82
Andrey <143511905+Iziumich@users.noreply.github.com>
2025-02-25 16:37:47 +0300

    Update PersonRepository.java

--- src/main/java/ru/netology/dao_hibernate/PersonRepository.java
+++ src/main/java/ru/netology/dao_hibernate/PersonRepository.java
@@ -6,9 +6,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
     @Query("SELECT p FROM Person p WHERE p.cityOfLiving = :city")
     List<Person> findByCityOfLiving(@Param("city") String city);
+
+    @Query("SELECT p FROM Person p WHERE p.age < :age ORDER BY p.age ASC")
+    List<Person> findByAgeLessThanOrderByAgeAsc(@Param("age") Integer age);
+
+    @Query("SELECT p FROM Person p WHERE p.name = :name AND p.surname = :surname")
+    Optional<Person> findByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
